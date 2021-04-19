feab127f6f9f4e36b95e26f8af92f59a8cd75a8c
codeblurred <codeblurred@gmail.com>
2021-04-20 00:14:19 +0530

    vamsdock

--- section-2/multi-stage-example/build.sh
+++ section-2/multi-stage-example/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-echo Building lukondefmwila/react:build 
+echo Building vamsdock/react:build 
 
-docker build -t lukondefmwila:build . -f Dockerfile.build 
+docker build -t vamsdock:build . -f Dockerfile.build 
 
-docker create --name extract lukondefmwila:build
+docker create --name extract vamsdock:build
 
 docker cp extract:/app/build ./app 
 
 docker rm -f extract 
 
-echo Building lukondefmwila/react:latest
+echo Building vamsdock/react:latest
 
-docker build --no-cache -t lukondefmwila/react:latest . -f Dockerfile.main
+docker build --no-cache -t vamsdock/react:latest . -f Dockerfile.main
